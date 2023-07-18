69e6d5c9994c36e51adc37471e9bacdbfe649f91
Yatin Bhatti <yatin@percept.com>
2023-07-18 11:52:43 +0530

    changes in wordfence-waf file

--- public_html/wordfence-waf.php
+++ public_html/wordfence-waf.php
@@ -1,8 +1,8 @@
 <?php
 // Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
 
-if (file_exists('/home/965616.cloudwaysapps.com/ahjtqsthut/public_html/wp-content/plugins/wordfence/waf/bootstrap.php')) {
-	define("WFWAF_LOG_PATH", '/home/965616.cloudwaysapps.com/ahjtqsthut/public_html/wp-content/wflogs/');
-	include_once '/home/965616.cloudwaysapps.com/ahjtqsthut/public_html/wp-content/plugins/wordfence/waf/bootstrap.php';
+if (file_exists('/home/https://perceptsystems-bf6b9a55f718.herokuapp.com/ahjtqsthut/public_html/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/home/https://perceptsystems-bf6b9a55f718.herokuapp.com/ahjtqsthut/public_html/wp-content/wflogs/');
+	include_once '/home/https://perceptsystems-bf6b9a55f718.herokuapp.com/ahjtqsthut/public_html/wp-content/plugins/wordfence/waf/bootstrap.php';
 }
 ?>
